91c996084353055af522e1e359180d0de86090fc
rick5678 <henriquefrancoaraujo@gmail.com>
2021-09-30 20:37:45 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./assets/app.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+   
+  const [valorMostrado, novoValor] = useState("");
+
+  const operador = "";
+  const guardaValor = "";
+  
+  const calculaValor = (e) =>{
+    novoValor(valorMostrado.concat(e.target.name));
+  } 
+    
+  const resultado = () =>{
+    try{
+    novoValor(eval(valorMostrado).toString());
+    } catch(e){
+      novoValor("Erro");
+    }
+  } 
+
+  const apagar = () => {
+    novoValor(valorMostrado.slice(0, -1));
+  }
+
+  const limpar = () => {
+    novoValor("");
+  }
+
+    return (
+      <section className="container">
+        <form className="calculadora-container">
+          <input type="text" id="campo-calculo" value={valorMostrado}
+            className="form-control calculadora-input"
+            disabled="true" />
+          <div className="section-botoes">
+            <input name="0" value="0" onClick={calculaValor}
+              className="botao-calculadora btn btn-outline-secondary" />
+            <input name="1" value="1" onClick={calculaValor}
+              className="botao-calculadora btn btn-outline-secondary" />
+            <input name="2" value="2" onClick={calculaValor}
+              className="botao-calculadora btn btn-outline-secondary" />
+            <input name="3" value="3" onClick={calculaValor}
+              className="botao-calculadora btn btn-outline-secondary" />
+            <input name="4" value="4" onClick={calculaValor}
+              className="botao-calculadora btn btn-outline-secondary" />
+            <input name="5" value="5" onClick={calculaValor}
+              className="botao-calculadora btn btn-outline-secondary" />
+            <input name="6" value="6" onClick={calculaValor}
+              className="botao-calculadora btn btn-outline-secondary" />
+            <input name="7" value="7" onClick={calculaValor}
+              className="botao-calculadora btn btn-outline-secondary" />
+            <input name="8" value="8" onClick={calculaValor}
+              className="botao-calculadora btn btn-outline-secondary" />
+            <input name="9" value="9" onClick={calculaValor}
+              className="botao-calculadora btn btn-outline-secondary" />
+            <input name="*" value="x" onClick={calculaValor} id="operator"
+              className="botao-calculadora btn btn-outline-secondary" />
+            <input name="-" value="-" onClick={calculaValor} id="operator"
+              className="botao-calculadora btn btn-outline-secondary" />
+            <input name="+" value="+" onClick={calculaValor} id="operator"
+              className="botao-calculadora btn btn-outline-secondary" />
+            <input name="/" value="/" onClick={calculaValor} id="operator"
+              className="botao-calculadora btn btn-outline-secondary" />
+            <input name="." value="." onClick={calculaValor} id="operator"
+              className="botao-calculadora btn btn-outline-secondary" />
+            <input value="del" onClick={apagar} id="operator"
+              className="botao-calculadora btn btn-outline-secondary" />
+            <input value="limpar" onClick={limpar} id="limpar"
+              className="botao-calculadora btn btn-outline-secondary" />
+            <input value="=" onClick={resultado} id="igual"
+              className="botao-calculadora btn btn-outline-secondary" />
+          </div>
+        </form>
+      </section>
+    )
+  }
+
 
 export default App;
